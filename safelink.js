b446d21024ec9a41d3d581192110b31815272e84
vozdo <65387433+vozdo@users.noreply.github.com>
2020-05-15 15:22:37 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -28,7 +28,7 @@ if(x=="4"){xxx="how-modern-insurance-policy-systems-are.html"}
 if(x=="5"){xxx="how-to-make-money-trading-forex.html"}
 if(x=="6"){xxx="how-bitcoin-works.html"}
 if(x=="7"){xxx="how-does-bitcoin-mining-works.html"}
-if(x=="4"){xxx="what-is-traded-in-forex.html"}a_to_ve[i].href="https://jeniusguru.blogspot.com/2020/05/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="8"){xxx="what-is-traded-in-forex.html"}a_to_ve[i].href="https://jeniusguru.blogspot.com/2020/05/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
