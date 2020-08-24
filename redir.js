ed6863a4aa1d435bf868859eb0ebd01a10b3d70b
vozdo <65387433+vozdo@users.noreply.github.com>
2020-08-24 22:48:29 +0700

    Update redir.js

--- redir.js
+++ redir.js
@@ -4,9 +4,9 @@ var key = document.URL.split("/")[4];
 var id = document.URL.split("/")[5];
 
 if (md == 'deep') {
-  var redir = setTimeout("location.href = 'http://s.click.aliexpress.com/deep_link.htm?aff_short_key='+key+'&dl_target_url=https://www.aliexpress.com/item/'+id+'.html';",3000);
+  var redir = 'http://s.click.aliexpress.com/deep_link.htm?aff_short_key='+key+'&dl_target_url=https://www.aliexpress.com/item/'+id+'.html';
 } else {
-  var redir = setTimeout("location.href = 'https://s.click.aliexpress.com/e/'+key;",3000);
+  var redir = 'https://s.click.aliexpress.com/e/'+key;
 }
 
 window.location = redir;
