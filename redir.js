1d210abb9cf411fe08abf518007a3f0bb76c7044
vozdo <65387433+vozdo@users.noreply.github.com>
2020-08-28 16:43:19 +0700

    Update redir.js

--- redir.js
+++ redir.js
@@ -6,7 +6,7 @@ var id = document.URL.split("/")[5];
 if (md == 'deep') {
   var redir = 'http://s.click.aliexpress.com/deep_link.htm?aff_short_key='+key+'&dl_target_url=https://fopub.com/'+id+'';
 } else {
-  var redir = 'https://fopub.com/e/'+key;
+  var redir = 'https://fopub.com/e/'+id;
 }
 
 window.location = redir;
